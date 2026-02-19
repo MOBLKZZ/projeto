6a63b855659088a141f19b3079b9f32ca5f9583d
MOBLKZZ <gabrielmacedolkz@gmail.com>
2026-02-19 03:02:51 -0300

    Add new styles for cardapio and contato sections; refactor inicio and sobre styles
    - Created _cardapio.scss and _contato.scss for dedicated styling of the cardapio and contato sections.
    - Updated _inicio.scss to improve background image handling and removed unnecessary properties.
    - Refactored _sobre.scss to enhance layout and styling consistency.
    - Modified style.css to integrate new styles and improve overall structure.
    - Adjusted style.scss to include new imports and clean up comments.

--- src/coponents/Card/Card.jsx
+++ src/coponents/Card/Card.jsx
@@ -1,34 +1,38 @@
 import "./style.css"
 
-function Card({ image, title, style}) {
+function Card({ image, title, style, name}) {
 
     if (style === "card-1") {
         return (
             <div className="Card-1">
-                <img src={image} alt="" />
+                <img src={image} alt="" loading="lazy"/>
+                <h3>{name}</h3>
             </div>
         )
     } else if (style === "card-2") {
         return (
             <div className="Card-2">
-                <img src={image} alt="" />
+                <img src={image} alt="" loading="lazy"/>
+                <h3>{name}</h3>
             </div>
         )
     } else if (style === "card-3") {
         return (
             <div className="Card-3">
-                <img src={image} alt="" />
+                <img src={image} alt="" loading="lazy"/>
+        <h3>{name}</h3>
             </div>
         )
     } else if (style === "card-4") {
         return (
             <div className="Card-4">
-                <img src={image} alt="" />
+                <img src={image} alt="" loading="laze"/>
+                <h3>{name}</h3>
             </div>
         )
     }
     return (
-
+        
         <div className="Card">
             <img src={image} alt="" />
             <h1>{title}</h1>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
         <>
             <nav>
                 <ul>
-                    <div className="banner"><a href="/pedido"><img src="images/banner.png" alt="" /></a></div>
+                    <div className="banner"><img src="/images/banner.png" /></div>
                     <li><a href="#inicio">Inicio</a></li>
                     <li><a href="#sobre">Sobre</a></li>
                     <li><a href="#cardapio">Cardapio</a></li>
@@ -31,7 +31,7 @@ function Home() {
 
             <main>
                 <section id="inicio">
-                    <div className="foto"><img className="fundo-2" src="/images/background.webp" alt="" /></div>
+                    <div className="foto"></div>
                     <div className="Moldura">
                         <h1>SABOR TRADICIONAL</h1>
 
@@ -54,69 +54,88 @@ function Home() {
                     <div className="linha"></div>
                     <div className="linha"></div>
 
-                    <img className="pessoa" src="/images/pessoa.avif" />
+                    <img className="pessoa" src="/images/pessoa.avif" loading="lazy" />
 
                     <h1 data-aos="flip-up" data-aos-duration="1500" >Nossa História</h1>
 
-                    <div className="historia-conteudo">
-                        <p data-aos="fade-right" data-aos-duration="1500">
-                            Tudo começou em uma cozinha pequena, uma chapa emprestada e uma obsessão:
-                            <strong> criar o hambúrguer perfeito.</strong>
-                        </p>
-                        <p data-aos="fade-right" data-aos-duration="1500">
-                            A <strong>Los Burguer</strong> nasceu da vontade de resgatar o sabor real da carne
-                            grelhada no fogo, longe dos processos industriais. O que era apenas um encontro
-                            de amigos no final de semana transformou-se em nossa missão de vida.
-                        </p>
-                        <p data-aos="fade-right" data-aos-duration="1500">
-                            Selecionamos cada ingrediente a dedo — do pão brioche sempre fresco à nossa
-                            maionese secreta — para garantir que cada mordida conte uma história.
-                            Na Los Burguer, não servimos apenas comida; entregamos a nossa paixão em formato de blend.
-                        </p>
-                    </div>
-
-                    <div className="Continuacao-Historia">
-                        <p data-aos="fade-left" data-aos-duration="1500">
-                            Não é apenas sobre empilhar ingredientes; é sobre a harmonia entre eles.
-                            Nossa cozinha funciona como uma orquestra, onde o chiado da carne na chapa
-                            é a música principal.
-                        </p>
-
-                        <div>
-                            <p data-aos="fade-left" data-aos-duration="1500">
-                                Trabalhamos exclusivamente com <strong>produtores locais</strong>, garantindo
-                                que o tomate seja colhido no dia e que o nosso blend de carne nunca tenha
-                                visto o interior de um congelador.
+                    <div className="container-historia">
+                        <div className="historia-conteudo">
+                            <p data-aos="fade-right" data-aos-duration="1500">
+                                Tudo começou em uma cozinha pequena, uma chapa emprestada e uma obsessão:
+                                <strong> criar o hambúrguer perfeito.</strong>
+                            </p>
+                            <p data-aos="fade-right" data-aos-duration="1500">
+                                A <strong>Los Burguer</strong> nasceu da vontade de resgatar o sabor real da carne
+                                grelhada no fogo, longe dos processos industriais. O que era apenas um encontro
+                                de amigos no final de semana transformou-se em nossa missão de vida.
+                            </p>
+                            <p data-aos="fade-right" data-aos-duration="1500">
+                                Selecionamos cada ingrediente a dedo — do pão brioche sempre fresco à nossa
+                                maionese secreta — para garantir que cada mordida conte uma história.
+                                Na Los Burguer, não servimos apenas comida; entregamos a nossa paixão em formato de blend.
                             </p>
                         </div>
 
-                        <p data-aos="fade-left" data-aos-duration="1500">
-                            Queremos que a <strong>Los Burguer</strong> seja a extensão da sua casa.
-                            Um lugar onde o balcão é o palco de boas conversas e cada lanche servido
-                            é um convite para voltar amanhã.
-                        </p>
+                        <div className="Continuacao-Historia">
+                            <p data-aos="fade-left" data-aos-duration="1500">
+                                Não é apenas sobre empilhar ingredientes; é sobre a harmonia entre eles.
+                                Nossa cozinha funciona como uma orquestra, onde o chiado da carne na chapa
+                                é a música principal.
+                            </p>
+
+                            <div>
+                                <p data-aos="fade-left" data-aos-duration="1500">
+                                    Trabalhamos exclusivamente com <strong>produtores locais</strong>, garantindo
+                                    que o tomate seja colhido no dia e que o nosso blend de carne nunca tenha
+                                    visto o interior de um congelador.
+                                </p>
+                            </div>
+
+                            <p data-aos="fade-left" data-aos-duration="1500">
+                                Queremos que a <strong>Los Burguer</strong> seja a extensão da sua casa.
+                                Um lugar onde o balcão é o palco de boas conversas e cada lanche servido
+                                é um convite para voltar amanhã.
+                            </p>
+                        </div>
                     </div>
                 </section>
 
                 <section id="cardapio">
+                    <h1 data-aos="flip-up" data-aos-duration="1500" >Cardapio</h1>
                     <div className="Container-Card">
-                        <Card style="card-1" image="/images-card/burger.avif"></Card>
-                        <Card style="card-2" image="/images-card/drink.avif"></Card>
-                        <Card style="card-3" image="/images-card/milkshake.avif"></Card>
-                        <Card style="card-4" image="/images-card/cupcakes.avif"></Card>
+                        <Card style="card-1" image="/images-card/burger.avif" name="Burguers"></Card>
+                        <Card style="card-2" image="/images-card/drink.avif" name="Drinks"></Card>
+                        <Card style="card-3" image="/images-card/milkshake.avif" name="Shakes"></Card>
+                        <Card style="card-4" image="/images-card/cupcakes.avif" name="Sweets"></Card>
                     </div>
                 </section>
 
                 <section id="contato">
+                    <h1>Entre em Contato</h1>
+                    <div className="container-links">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fff" d="M7.03.084c-1.277.06-2.149.264-2.91.563a5.9 5.9 0 0 0-2.124 1.388a5.9 5.9 0 0 0-1.38 2.127C.321 4.926.12 5.8.064 7.076s-.069 1.688-.063 4.947s.021 3.667.083 4.947c.061 1.277.264 2.149.563 2.911c.308.789.72 1.457 1.388 2.123a5.9 5.9 0 0 0 2.129 1.38c.763.295 1.636.496 2.913.552c1.278.056 1.689.069 4.947.063s3.668-.021 4.947-.082c1.28-.06 2.147-.265 2.91-.563a5.9 5.9 0 0 0 2.123-1.388a5.9 5.9 0 0 0 1.38-2.129c.295-.763.496-1.636.551-2.912c.056-1.28.07-1.69.063-4.948c-.006-3.258-.02-3.667-.081-4.947c-.06-1.28-.264-2.148-.564-2.911a5.9 5.9 0 0 0-1.387-2.123a5.9 5.9 0 0 0-2.128-1.38c-.764-.294-1.636-.496-2.914-.55C15.647.009 15.236-.006 11.977 0S8.31.021 7.03.084m.14 21.693c-1.17-.05-1.805-.245-2.228-.408a3.7 3.7 0 0 1-1.382-.895a3.7 3.7 0 0 1-.9-1.378c-.165-.423-.363-1.058-.417-2.228c-.06-1.264-.072-1.644-.08-4.848c-.006-3.204.006-3.583.061-4.848c.05-1.169.246-1.805.408-2.228c.216-.561.477-.96.895-1.382a3.7 3.7 0 0 1 1.379-.9c.423-.165 1.057-.361 2.227-.417c1.265-.06 1.644-.072 4.848-.08c3.203-.006 3.583.006 4.85.062c1.168.05 1.804.244 2.227.408c.56.216.96.475 1.382.895s.681.817.9 1.378c.165.422.362 1.056.417 2.227c.06 1.265.074 1.645.08 4.848c.005 3.203-.006 3.583-.061 4.848c-.051 1.17-.245 1.805-.408 2.23c-.216.56-.477.96-.896 1.38a3.7 3.7 0 0 1-1.378.9c-.422.165-1.058.362-2.226.418c-1.266.06-1.645.072-4.85.079s-3.582-.006-4.848-.06m9.783-16.192a1.44 1.44 0 1 0 1.437-1.442a1.44 1.44 0 0 0-1.437 1.442M5.839 12.012a6.161 6.161 0 1 0 12.323-.024a6.162 6.162 0 0 0-12.323.024M8 12.008A4 4 0 1 1 12.008 16A4 4 0 0 1 8 12.008" /></svg>
+                            <label>Instagram</label>
+                        </div>
+
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fff" d="M12.525.02c1.31-.02 2.61-.01 3.91-.02c.08 1.53.63 3.09 1.75 4.17c1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97c-.57-.26-1.1-.59-1.62-.93c-.01 2.92.01 5.84-.02 8.75c-.08 1.4-.54 2.79-1.35 3.94c-1.31 1.92-3.58 3.17-5.91 3.21c-1.43.08-2.86-.31-4.08-1.03c-2.02-1.19-3.44-3.37-3.65-5.71c-.02-.5-.03-1-.01-1.49c.18-1.9 1.12-3.72 2.58-4.96c1.66-1.44 3.98-2.13 6.15-1.72c.02 1.48-.04 2.96-.04 4.44c-.99-.32-2.15-.23-3.02.37c-.63.41-1.11 1.04-1.36 1.75c-.21.51-.15 1.07-.14 1.61c.24 1.64 1.82 3.02 3.5 2.87c1.12-.01 2.19-.66 2.77-1.61c.19-.33.4-.67.41-1.06c.1-1.79.06-3.57.07-5.36c.01-4.03-.01-8.05.02-12.07" /></svg>
+                            <label>TikTok</label>
+                        </div>
+
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fff" d="M21.543 7.104c.015.211.015.423.015.636c0 6.507-4.954 14.01-14.01 14.01v-.003A13.94 13.94 0 0 1 0 19.539a9.88 9.88 0 0 0 7.287-2.041a4.93 4.93 0 0 1-4.6-3.42a4.9 4.9 0 0 0 2.223-.084A4.926 4.926 0 0 1 .96 9.167v-.062a4.9 4.9 0 0 0 2.235.616A4.93 4.93 0 0 1 1.67 3.148a13.98 13.98 0 0 0 10.15 5.144a4.929 4.929 0 0 1 8.39-4.49a9.9 9.9 0 0 0 3.128-1.196a4.94 4.94 0 0 1-2.165 2.724A9.8 9.8 0 0 0 24 4.555a10 10 0 0 1-2.457 2.549" /></svg>
+                            <label>Twiter</label>
+                        </div>
+                    </div>
+
                     <div className="hamburguer">
-                        <img src="/images/hamburguer.webp" />
-                        <img src="/images/olho-1.png" alt="" />
-                        <img src="/images/olho-2.png" alt="" />
+                        <img className="burguer" src="/images/hamburguer.webp" />
+                        <img className="olho-esquerdo" src="/images/olho-1.png" />
+                        <img className="olho-direito" src="/images/olho-2.png" />
                     </div>
                 </section>
-
             </main>
-            <footer>feito por mim</footer>
         </>
     );
 }
